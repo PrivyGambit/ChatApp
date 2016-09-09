7c35dc62a70806dbe37e4eb0d83869114529e1f5
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-09-09 16:37:49 +0900

    update

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -2,74 +2,74 @@ import { decisionsExpirationLength } from 'config/constants'
 import restrictions from 'config/restrictions'
 
 export function formatUserInfo (name, avatar, uid) {
-  return {
-    name,
-    avatar,
-    uid,
-    type: 'normal'
-  }
+    return {
+        name,
+        avatar,
+        uid,
+        type: 'normal'
+    }
 }
 
 export function formatDecision (title, firstDecisionText, secondDecisionText, user) {
-  return {
-    timestamp: Date.now(),
-    author: user,
-    title,
-    firstOption: {
-      text: firstDecisionText,
-      selectedCount: 0,
-    },
-    secondOption: {
-      text: secondDecisionText,
-      selectedCount: 0,
+    return {
+        timestamp: Date.now(),
+        author: user,
+        title,
+        firstOption: {
+            text: firstDecisionText,
+            selectedCount: 0,
+        },
+        secondOption: {
+            text: secondDecisionText,
+            selectedCount: 0,
+        }
     }
-  }
 }
 
-export function formatTimestamp (timestamp) {
-  const date = new Date(timestamp)
-  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+export function formatTimestamp ( timestamp ) {
+    const date = new Date(timestamp)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 }
 
-function getMilliseconds (timestamp) {
-  return new Date().getTime() - new Date(timestamp).getTime()
+function getMilliseconds ( timestamp ) {
+    return new Date().getTime() - new Date(timestamp).getTime()
 }
 
 export function decisionsAreStale (timestamp) {
-  return getMilliseconds(timestamp) > decisionsExpirationLength
+    return getMilliseconds(timestamp) > decisionsExpirationLength
 }
 
 export function formatRoomName ( roomName ) {
-  return {
-    roomName: roomName,
-    latestUpdateTime: Date.now(),
-    newContent: {
-      user: '',
-      chat: '',
+    return {
+        roomName: roomName,
+        latestUpdateTime: Date.now(),
+        newContent: {
+            user: '',
+            chat: '',
+        }
     }
-  }
 }
 
 export function formatChat ( chat ) {
-  return {
-    type: chat.type,
-    content: chat.text,
-    user: chat.user,
-    avatar: chat.avatar,
-    timestamp: Date.now()
-  }
+    return {
+        type: chat.type,
+        content: chat.text,
+        user: chat.user,
+        avatar: chat.avatar,
+        timestamp: Date.now()
+    }
 }
 
 export function formatFile ( chat ) {
-  return {
-    type: chat.type,
-    user: chat.user,
-    avatar: chat.avatar,
-    timestamp: Date.now()
-  }
+    return {
+        type: chat.type,
+        user: chat.user,
+        avatar: chat.avatar,
+        timestamp: Date.now()
+    }
 }
 
 export function filterText ( str ) {
-  const rgx = new RegExp(restrictions.join("|"), "gi");
-  return str.replace(rgx, "****");
+    const rgx = new RegExp(restrictions.join("|"), "gi");
+    return str.replace(rgx, "****");
 }
